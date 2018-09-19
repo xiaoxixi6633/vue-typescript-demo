4c6d04ef39137e287401e907a6cbec7a31445ed4
ex_mianmianwang@jieyuechina.com <281419583@qq.com>
2018-09-19 09:44:04 +0800

    feat home component

---
+++ src/components/home/home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home-wrapper">
+      <head-component></head-component>
+      <section-hd></section-hd>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import headComponent from '@/components/headComponent/headComponent.vue';
+import sectionHd from '@/components/sectionHd/index.vue';
+@Component({
+  components: {
+    headComponent,
+    sectionHd
+  }
+})
+
+export default class home extends Vue {
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
