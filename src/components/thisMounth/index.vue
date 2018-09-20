318f4fada535f7ea9cee575a8ac0fac9eacd04be
ex_mianmianwang@jieyuechina.com <281419583@qq.com>
2018-09-20 15:20:01 +0800

    add thisMounth file

---
+++ src/components/thisMounth/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="home-section-wrapper">
+      <div class="home-section">
+        <div class="home-section-hd">实际赎回金额 (元)</div>
+        <div class="home-section-content">
+          <div>565,566</div>
+          <div>233,355</div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import {State} from 'vuex-class';
+@Component
+export default class homeSection extends Vue {
+  @State login: boolean;
+};
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
