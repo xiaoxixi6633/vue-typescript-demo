19d317ce1ac3fdc340ae09e0d38d7837c7ddb98e
ex_mianmianwang@jieyuechina.com <281419583@qq.com>
2018-09-20 15:19:04 +0800

    add today 今日

---
+++ src/components/today/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="today-wrapper">
+      <section-hd></section-hd>
+      <home-section></home-section>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import {State} from 'vuex-class';
+import sectionHd from '@/components/sectionHd/index.vue';
+import homeSection from '@/components/homeSection/index.vue'
+@Component({
+  components: {
+    sectionHd,
+    homeSection
+  }
+})
+export default class today extends Vue {
+  @State login: boolean;
+};
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
