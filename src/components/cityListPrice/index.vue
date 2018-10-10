fb8b59877f00cc59481bca36354c9234e4119a35
ex_mianmianwang@jieyuechina.com <281419583@qq.com>
2018-10-10 21:13:43 +0800

    add cityListPrice

--- src/components/cityListPrice/index.vue
+++ src/components/cityListPrice/index.vue
@@ -6,16 +6,18 @@
         <th>金额（万）</th>
         <th>时间</th>
       </tr>
-      <!-- <tr v-for="(item, index) in listData" :key="index">
+      <tr v-for="(item, index) in modules" :key="index">
         <td>{{item.id}}</td>
-        <td>{{item.title}}</td>
-        <td>{{item.sendTime}}</td>
-      </tr> -->
+        <td>{{item.price}}</td>
+        <td>{{item.time}}</td>
+      </tr>
     </table>
     <div class="bottom-fixed" v-show="bottomFixed">-&nbsp;向前金服&nbsp;.向前向未来&nbsp;-</div>
     <div slot="bottom" class="mint-loadmore-bottom" v-show="bottomLoading">
-      <div class="icon"><!-- <cube-loading :size="28" color='#4E97FF'></cube-loading> --></div>
-      <span>正在加载...</span>
+      <div class="el-loading-spinner">
+        <svg viewBox="25 25 50 50" class="circular"><circle cx="50" cy="50" r="20" fill="none" class="path"></circle></svg>
+        <p class="el-loading-text">加载中......</p>
+      </div>
     </div>
     <div v-show="isNomleBottom && !isNoData" class="no-data mint-loadmore-bottom" style="padding-bottom:40px;">
       <span class="line line1"></span>我是有底线的<span class="line line2"></span>
@@ -31,122 +33,71 @@
   //import Vue from 'vue';
   //import Component from 'vue-class-component';
   import {Component, Provide,Vue} from 'vue-property-decorator';
-  import { State } from 'vuex-class';
+  //import { State } from 'vuex-class';
   //import { Action } from 'vuex-class';
+  import { State, Action, Getter } from 'vuex-class';
   import axios from 'axios'
+  import { Loading } from 'element-ui';
   @Component
   export default class cityListPrice extends Vue {
-    @Provide() listData:string[]     = [];
-    @State activities: StoreState.activity[]
-    @Provide() bottomLoading:boolean = false;
+    @State modules: StoreState.module[];
+    @Provide() bottomLoading:boolean = true;
     @Provide() isNomleBottom:boolean = false;// 判断到底部标识
     @Provide() isNoData:boolean      = false;//暂无数据
     @Provide() goTopShow:boolean     = false;// 返回顶部
     @Provide() isRequested           = false;//请求成功后执行下次请求表示
     @Provide() page:number           = 1;
     @Provide() bottomFixed:boolean   = true;
-    created(){
-      //this.getDataList();
-      //this.listData =
-    }
+    @Action initAjax: () => void;
+    // created(){
+    //   Loading.service({
+    //     lock: true,
+    //     text: '加载中......',
+    //     background: 'none'
+    //   })
+    // }
     
     mounted(){
       this.$nextTick(()=>{
         window.scrollTo(0, 0);
         this.addListener();
+        this.initAjax();
       })
     }
 
-    // getDataList(){
-    //   if(this.isRequested) {
-    //       return false;
-    //   }
-    //   this.isRequested = true;
-    //     //let token = this.userid;
-    //     axios.post({
-    //       method: 'post',
-    //       url: '/mc-message/message/notice/list',
-    //       dataType: 'json', 
-    //       headers: {'Content-Type': 'application/json;charset=UTF-8'},
-    //       data:JSON.stringify({
-    //         token: 'sdjfksdjflkdsf',
-    //         currentPage: this.page
-    //       })
-    //     }).then(res=> {  // 请求成功
-    //       if(res.data.code!=500){
-    //         //this.saveLoadingShow(false);
-    //       }
-    //       if(res.data.code == 200){
-    //         // 判断没有任何数据的时候显示的是无数据提示
-    //         if(res.data.data && res.data.data.length<1 && this.page===1){
-    //           this.isNoData = true;
-    //            return;
-    //         }
-    //         // 加载到最后一页没有数据的情况
-    //         else if(res.data.data.length<1 && this.page !==1){
-    //           this.isNomleBottom = true;
-    //           this.bottomLoading = false;
-    //           this.bottomFixed = true;
-    //           return false;
+    loadBottom() {// 加载更多数据
+      if (this.isNomleBottom) return;
+      this.bottomLoading = true;
+      setTimeout(()=> {
+        //this.getDataList();
+      },500);
+    }
 
-    //           // 返回有数据就把 原dataPlanLists和返回数据合并数组
-    //         } else {
-    //           this.listData = this.listData.concat(res.data.data);
-    //           this.page++;
-    //         }
+    addListener(){
+      window.addEventListener('scroll', this.handleScroll);
+    }
 
-    //       } else if(res.data.code == 407){ //被踢出登录，返回登录页
-    //         //this.$logOut();
-    //       } else if(res.data.code == 301){//7天自动退出登录
-    //         // this.$logOut({
-    //         //   rememberpwd:'1' 
-    //         // }); 
-    //       } else {
-    //         //this.$toast(res.data.msg);
-    //       }
-    //       setTimeout(()=>{
-    //         //this.isRequested = false;
-    //       },500);
-            
-    //     }).catch(function(err){// 请求失败
-    //       console.log(err);
-    //       //this.isRequested = false;
-    //     })
-    //   }
+    removeListener(){
+      window.removeEventListener('scroll', this.handleScroll);
+    }
 
-      loadBottom() {// 加载更多数据
-        if (this.isNomleBottom) return;
-        this.bottomLoading = true;
-        setTimeout(()=> {
-          //this.getDataList();
-        },500);
+    handleScroll () {
+      const scrollTop:number = window.pageYOffset || document.documentElement.scrollTop;
+      const scrollHeight:number = document.body.scrollHeight || document.documentElement.scrollHeight;
+      const windowHeight:number = document.body.clientHeight || document.documentElement.clientHeight;
+      //滚动加载更多
+      if(scrollTop + windowHeight >= scrollHeight-100){ 
+        this.bottomFixed = false;
+        this.loadBottom();
       }
-
-      addListener(){
-        window.addEventListener('scroll', this.handleScroll);
+      //回到顶部
+      if(scrollTop > 400){
+        this.goTopShow = true;
+        //this.scrollTop = scrollTop;
+      } else{
+        this.goTopShow = false;
       }
-
-      removeListener(){
-        window.removeEventListener('scroll', this.handleScroll);
-      }
-
-      handleScroll () {
-        const scrollTop:number = window.pageYOffset || document.documentElement.scrollTop;
-        const scrollHeight:number = document.body.scrollHeight || document.documentElement.scrollHeight;
-        const windowHeight:number = document.body.clientHeight || document.documentElement.clientHeight;
-        //滚动加载更多
-        if(scrollTop + windowHeight >= scrollHeight-100){ 
-          this.bottomFixed = false;
-          this.loadBottom();
-        }
-        //回到顶部
-        if(scrollTop > 400){
-          this.goTopShow = true;
-          //this.scrollTop = scrollTop;
-        } else{
-          this.goTopShow = false;
-        }
-      }
+    }
   };
 </script>
 
