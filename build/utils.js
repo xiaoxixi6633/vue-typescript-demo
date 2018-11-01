7e937fdae93937349ffb5a0fb354510fd280dc33
ex_mianmianwang@jieyuechina.com <281419583@qq.com>
2018-11-01 11:13:07 +0800

    add css background images bug

--- build/utils.js
+++ build/utils.js
@@ -51,6 +51,7 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
+        publicPath: '../../',
         fallback: 'vue-style-loader'
       })
     } else {
